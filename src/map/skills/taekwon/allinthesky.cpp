098c84aae4958487dbb2caf18e2d44e3a2d2cfd8
rhueh <huehnerschulte.raffael@gmail.com>
2026-09-04 07:48:50 +0200

    SafaRO: Klassenbalance - fehlendes RE_LVL_DMOD beim Sky Emperor, Trefferzahlen der 14 schwachen Klassen angehoben

--- src/map/skills/taekwon/allinthesky.cpp
+++ src/map/skills/taekwon/allinthesky.cpp
@@ -20,11 +20,22 @@ void SkillAllInTheSky::castendDamageId(block_list* src, block_list* target, uint
 	skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
 }
 
-void SkillAllInTheSky::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
+// SafaRO: Der Parameter heisst hier bewusst skillratio und nicht
+// base_skillratio wie in der Oberklasse. RE_LVL_DMOD (config/const.hpp)
+// schreibt fest auf eine Variable dieses Namens - mit base_skillratio
+// uebersetzt die Datei nicht. Genau daran ist die Zeile urspruenglich
+// gescheitert; roundtrip.cpp zeigt dasselbe Muster.
+void SkillAllInTheSky::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	const status_data* sstatus = status_get_status_data(*src);
 
-	base_skillratio += -100 + 250 + 1200 * skill_lv;
-	base_skillratio += 5 * sstatus->pow;
+	skillratio += -100 + 250 + 1200 * skill_lv;
+	skillratio += 5 * sstatus->pow;
+	// SafaRO: Diese Zeile fehlte. SKE_ALL_IN_THE_SKY war die EINZIGE
+	// Fertigkeit einer vierten Klasse ohne den Levelbonus - auf
+	// Stufe 500 blieb sie bei 13.150 Prozent stehen, waehrend jede
+	// andere Fertigkeit verfuenffacht wird. Der Vorzeigeskill des
+	// Sky Emperor war damit schwaecher als seine Nebenfertigkeiten.
+	RE_LVL_DMOD(100);
 }
 
 void SkillAllInTheSky::modifyDamageData(Damage& dmg, const block_list& src, const block_list& target, uint16 skill_lv) const {
