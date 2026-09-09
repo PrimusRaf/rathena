1e817f12390118d88a7f4105cc63d4d5371a6aa5
rhueh <huehnerschulte.raffael@gmail.com>
2026-09-09 12:02:46 +0200

    SafaRO: PACKETVER 20250716 fest in defines_pre.hpp

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -9,6 +9,13 @@
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
 
-
+// SafaRO: Client-Paketversion fest im Quelltext. Muss zu docker/.env
+// (PACKETVER) passen. Grund: Am 09.09.2026 wurde ./configure ohne
+// --enable-packetver aufgerufen, der Server lief mit rAthenas
+// Standardwert, und alle Spieler erschienen als Novice. Mit diesem
+// Fallback ist ein vergessenes configure-Flag folgenlos.
+#ifndef PACKETVER
+	#define PACKETVER 20250716
+#endif
 
 #endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
